31aa82d79640330ee0f52874486211d96ea10c47
Madhu Babu <17622671+Pandimadhubabu@users.noreply.github.com>
2020-09-10 14:42:00 +0530

    Update bundle.js

--- src/bundle.js
+++ src/bundle.js
@@ -1,15 +1,11 @@
-import React from 'react'
-import {render} from 'react-dom'
-import {Provider} from 'react-redux'
-import configureStore from './redux/configureStore'
-import App from './components/app'
+import React from 'react';
+import { render } from 'react-dom';
+import { Provider } from 'react-redux';
+import configureStore from './redux/configureStore';
+import App from './components/app';
 
-// Create a fresh store 
-const store = configureStore()
-
+const store = configureStore();
 render(
-  <Provider store={store} >
-     <App />
-  </Provider>,
+  <Provider store={store} > <App /> </Provider>,
   document.querySelector('#app')
-)
+);
